3c0d28da69a14b4a188efd4ebd44efb0788f4e54
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2026-07-12 16:59:13 -0400

    feat: implement basic LinkedList class with insertion and deletion operations

--- Assignments/Lab_1/LinkedList.cpp
+++ Assignments/Lab_1/LinkedList.cpp
@@ -48,71 +48,113 @@ public:
       }
     }
 
-    void deleteAtBack(){
-      //Check if(List = Empty)
-      if(head == nullptr){
+    void deleteAtBack() {
+      // Check if(List = Empty)
+      if (head == nullptr) {
         return;
       }
-      //Check if- It only contain: One Node
-      if(head == tail){
-        //Set Both headers = NULL
+      // Check if- It only contain: One Node
+      if (head == tail) {
+        // Set Both headers = NULL
         head = nullptr;
         tail = nullptr;
         return;
       }
 
-      //FIND (2nd to Last) Node
-        ///Create Temp - Pointer
-      Node * tempCurrent = head; //set to head- so we can travel the list from head to tail 
-      //While(State: Tell (Temp-Pointer)- to save a spot in line) - set = tail
-      while(tempCurrent -> next!= tail){
-        //Tell (Temp Pointer to go next inline)
-        tempCurrent = tempCurrent -> next;
+      // FIND (2nd to Last) Node
+      /// Create Temp - Pointer
+      Node *tempCurrent =
+          head; // set to head- so we can travel the list from head to tail
+      // While(State: Tell (Temp-Pointer)- to save a spot in line) - set = tail
+      while (tempCurrent->next != tail) {
+        // Tell (Temp Pointer to go next inline)
+        tempCurrent = tempCurrent->next;
       }
-      //Delete Last Node(Elements)
+      // Delete Last Node(Elements)
       delete tail;
-      //Tell Tail: to move to where the current-pointer is standing
-      //Set Tail to where the current pointer is  
-      tail = tempCurrent;//Now the last spot in the LIST
-      //Tell tail to not allow anymore ELEMENTS AFTER THEM
-       tempCurrent -> next = nullptr;
+      // Tell Tail: to move to where the current-pointer is standing
+      // Set Tail to where the current pointer is
+      tail = tempCurrent; // Now the last spot in the LIST
+      // Tell tail to not allow anymore ELEMENTS AFTER THEM
+      tempCurrent->next = nullptr;
     }
 
-    void deleteAtFront(){
-      Node * backdoorTemp = head;
-      if(head == nullptr){
+    void deleteAtFront() {
+      Node *backdoorTemp = head;
+      if (head == nullptr) {
         return;
       }
 
-      head = head -> next;
+      head = head->next;
 
-      if(head == nullptr){
-          tail == nullptr;
+      if (head == nullptr) {
+        tail == nullptr;
       }
-      
+
       delete backdoorTemp;
     }
 
+    // delete_by_value(int val) Delete the first occurrence of the provided
+    // value.
+    void delete_by_value(int val)() {
 
-    void display(){
-      if(head == nullptr){
+      // Is List = Empty?
+      if (head == nullptr) {
+        cout << "List is Empty" << endl;
+        return;
+      }
+
+      // Is there a Value at front? is there a val at the very front
+      if (head->val == val) {
+        Node *tempPointer = head;
+        // Move the Head to the next vlaue
+        head = head->next;
+        // Check if List = Empty
+        if (head == nullptr) {
+          tail = nullptr;
+        }
+        delete tempPointer;
+        return;
+      }
+
+      // Setup Tracking Pointers
+      Node *previousTrack = head;
+      Node *currentTrack = head->next;
+
+      // Travel and Delete
+      while (currentTrack != nullptr) {
+        if (currentTrack->val == val) {
+          previousTrack->next = currentTrack->next;
+
+          if (currentTrack->next == nullptr) {
+            tail = previousTrack;
+          }
+          delete currentTrack;
+          return;
+        } else {
+          previousTrack = currentTrack;
+          currentTrack = currentTrack->next;
+        }
+      }
+    }
+
+    void display() {
+      if (head == nullptr) {
         cout << "List Is Empty:" << endl;
         return;
       }
 
-      Node* displayPacket = head;
-      while(displayPacket != nullptr){
-        cout << displayPacket -> val;
-      
+      Node *displayPacket = head;
+      while (displayPacket != nullptr) {
+        cout << displayPacket->val;
 
-      if(displayPacket -> next != nullptr){
-        cout << "->" << endl;
+        if (displayPacket->next != nullptr) {
+          cout << "->" << endl;
+        }
+
+        displayPacket = displayPacket->next;
       }
-
-      displayPacket = displayPacket -> next;
     }
-  }
-  cout << endl;
-
+    cout << endl;
   }
 };
