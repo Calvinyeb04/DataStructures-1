55268e8113afb1d3e1cddb5576963e38e4b86642
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2026-07-16 19:15:58 -0400

    feat: implement ArrayStack dynamic resizing and stack operations, and add circular queue boilerplate

--- Assignments/Lab_2/ArrayStacks.cpp
+++ Assignments/Lab_2/ArrayStacks.cpp
@@ -1,41 +1,66 @@
 #include <iostream>
+#include LinkedList
 using namespace std;
-#include Linkedlist.cpp;
 
 class arrayStack{
     private:
-        int * array;
-        int capacity;
-        int top;
+        int* array;
+        int orignalCapacity; // - Max Items The Array Can Hole
+         int top; // - The Top Item - TRACKER
+
     public:
-        arrayStack(int Originalcapacity){
-            capacity = Originalcapacity;
-            arrayStack = new int[capacity];
+        arrayStack(int intitalCapacity){
+            orignalCapacity = intitalCapacity; // - Set The Orignal Cap to the Int one 
+            array = new int[orignalCapacity]; // - Tell the Array to Create the Space for the Origanl Cap
             top = -1;
         }
 
-       void push(int val){
-        // 1. Check if array (stack) is full
-        if(top == capacity - 1){
-            // 2. Create a new array double the size
-            int newCapacity = capacity * 2;
-            int* newArray = new int[newCapacity];
-            
-            // 3. Copy all data 
-            for(int i = 0; i < capacity; i++){
-                newArray[i] = array[i];
+        //Methods
+        void push(int val){
+            if(top == orignalCapacity -1){
+                int newCapacity = orignalCapacity * 2 ;// - Create A New Capacity Then Multiply by 2
+                int* newArray = new int[newCapacity]; //then set it to the new capacity 
+                for(int i = 0; i < newCapacity; i++){ 
+                    newArray[i] = array[i]; // - Copy all the Old Data into the New One
+                }
+
+                delete[]array; // - Delete Old Array
+                //Redirect Old Pointer to the new Bigger One
+                array = newArray; // Old Array to New One;
+                orignalCapacity = newCapacity;
             }
-
-            // 4. Delete the old array
-            delete[] array;
-
-            // 5. Update the old variables to point to the new ones
-            array = newArray;
-            capacity = newCapacity;
+             //put at top
+                array[++top] = val;
         }
 
-        // 6. Push the new value (This MUST be outside the if block!)
-        array[++top] = val;
-    }
+        void pop(){
+            //is the Stack Empty;  //- To Tracker(Top) has to point to a spot thats empty. // ARRAY SLOTs ARE NUmBERED starting from 0
+            if(top == -1){
+                cout << "The List Empty :)" << endl;
+                return;
+            }
+            //Reduce Top(index) to remove last Node
+            --top;
+        }
+
+        int peek(){
+            if (top == -1){
+                return -1; //Beucase of Method Declearation(int).
+            }
+            return array[top]; // - Return data at current(TOP index)
+        }
+
+        void display(){
+            if(top == -1){
+                cout << "Nothing To Display"
+                return;
+            }
+            //Loop Throught the entire List
+            for(int i = 0 ; i >= top; i--){
+                cout << array[i] << "" << endl; // Print each item stored in the array 
+            }
+        }
+        
+
 
 }
