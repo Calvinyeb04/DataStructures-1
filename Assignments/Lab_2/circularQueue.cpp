c20682c5cf7ed66df35d7919a015a5198c218be5
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2026-07-16 21:42:45 -0400

    STARTED: implement circular queue with dynamic resizing logic

--- Assignments/Lab_2/circularQueue.cpp
+++ Assignments/Lab_2/circularQueue.cpp
@@ -1,1 +1,45 @@
-w
+#include <iostream>
+#include LinkedList
+using namespace std;
+
+class circularQueue {
+private:
+  int *array;
+  int capacity; // Max Items - The Queue Can Hold
+  int size;     // - Items inside the Queue
+  int front;    // Index of - Oldest Item - (Next one to be Removed)
+  int rear;     // Index - Where the (Next New Item) will be inserted;
+public:
+  circularQueue(int intitalCapacity) {
+    capacity = intitalCapacity;
+    array = new int[capacity];
+    size = 0;
+    front = 0;
+    rear = 0;
+  }
+
+  void enqueue(int val) {
+    if (size == capacity) {
+      int newCapacity = capacity * 2; //  - Double The Capacity Size
+      int *newArray = new int[newCapacity];
+
+      // - Loop checking if The Size is not full
+      for (int i = 0; i < size; i++) {
+        newArray[i] =
+            array[(front + i) % capacity]; // - Copy The Items from Old To New
+      }
+
+      delete[] array; // - Delete Old Array;
+      array = newArray;
+      capacity = newCapacity;
+      front = 0;
+      rear = size;
+    }
+
+    array[rear] = array[val];
+
+    rear = (rear + 1) % capacity;
+
+    front - 1; // Reduce Counnt by 1
+  }
+}
